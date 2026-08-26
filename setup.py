cd96916939d3bb90253918060956461c13bf84ef
Remi <remi.poree@laas.fr>
2026-08-26 20:49:31 +0200

    :arrow_up: update dependencies

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
     ],
-    install_requires=['setuptools'],
+    install_requires=['setuptools', 'opencv-python'],
     zip_safe=True,
     maintainer='rporee',
     maintainer_email='remi.poree@laas.fr',
